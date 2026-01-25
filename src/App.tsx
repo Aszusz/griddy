147101e8588d02c2169ce3d15db6f80b06bd3113
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2026-01-25 16:29:43 +0100

    chore: add shadcn/ui with Button component and configure Tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
     <div className="py-8 text-center">
       <h1 className="text-4xl font-bold">No Delulu is the Solulu!</h1>
+      <div className="flex min-h-svh flex-col items-center justify-start py-4">
+        <Button>Stay Sane!</Button>
+      </div>
     </div>
   )
 }
