e29edc23f9075f8c74bcbc747493b8d68da7e721
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2026-01-26 10:42:32 +0100

    ui: increase grid dot and crosshair visibility

--- src/constants.ts
+++ src/constants.ts
@@ -6,9 +6,9 @@ export const SHAPE_FILL = '#3b82f6'
 export const SHAPE_STROKE = '#60a5fa'
 export const PREVIEW_FILL = '#3b82f680'
 
-export const GRID_DOT_COLOR = 'rgba(255, 255, 255, 0.13)'
+export const GRID_DOT_COLOR = 'rgba(255, 255, 255, 0.25)'
 export const GRID_DOT_RADIUS = 1
-export const CROSSHAIR_COLOR = 'rgba(34, 211, 238, 0.2)'
+export const CROSSHAIR_COLOR = 'rgba(34, 211, 238, 0.25)'
 export const CROSSHAIR_SIZE = 17
 export const CROSSHAIR_CENTER_RADIUS = 3
 
